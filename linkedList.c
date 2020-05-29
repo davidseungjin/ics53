4ebaacb4b5b33cc135b685acc2ef8d8373716110
davidseungjin <seungl21@uci.edu>
2020-05-29 09:40:23 +0000

    work done so far

---
+++ linkedList.c
@@ -0,0 +1,315 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define INT_MODE 0
+#define STR_MODE 1
+
+/*
+ * Structre for each node of the linkedList
+ *
+ * value - a pointer to the data of the node. 
+ * next - a pointer to the next node in the list. 
+ */
+typedef struct node {
+    void* value;
+    int fd;
+    struct node* next;
+} node_t;
+
+/*
+ * Structure for the base linkedList
+ * 
+ * head - a pointer to the first node in the list. NULL if length is 0.
+ * length - the current length of the linkedList. Must be initialized to 0.
+ * comparator - function pointer to linkedList comparator. Must be initialized!
+ */
+typedef struct list {
+    node_t* head;
+    int length;
+    /* the comparator uses the values of the nodes directly (i.e function has to be type aware) */
+    int (*comparator)(void*, void*);
+} List_t;
+
+/* 
+ * Each of these functions inserts the reference to the data (valref)
+ * into the linkedList list at the specified position
+ *
+ * @param list pointer to the linkedList struct
+ * @param valref pointer to the data to insert into the linkedList
+ */
+void insertRear(List_t* list, void* valref, int fd);
+void insertFront(List_t* list, void* valref, int fd);
+void insertInOrder(List_t* list, void* valref, int fd);
+
+/*
+ * Each of these functions removes a single linkedList node from
+ * the LinkedList at the specfied function position.
+ * @param list pointer to the linkedList struct
+ * @return a pointer to the removed list node
+ */ 
+void* removeFront(List_t* list);
+void* removeRear(List_t* list);
+void* removeByIndex(List_t* list, int n);
+
+/* 
+ * Free all nodes from the linkedList
+ *
+ * @param list pointer to the linkedList struct
+ */
+void deleteList(List_t* list);
+
+/*
+ * Traverse the list printing each node in the current order.
+ * @param list pointer to the linkedList strut
+ * @param mode STR_MODE to print node.value as a string,
+ * INT_MODE to print node.value as an int
+ */
+void printList(List_t* list, char mode);
+
+/* find fd by name */
+int find_fd_by_name(List_t* list, char* name);
+
+#endif
+/*
+    What is a linked list?
+    A linked list is a set of dynamically allocated nodes, arranged in
+    such a way that each node contains one value and one pointer.
+    The pointer always points to the next member of the list.
+    If the pointer is NULL, then it is the last node in the list.
+
+    A linked list is held using a local pointer variable which
+    points to the first item of the list. If that pointer is also NULL,
+    then the list is considered to be empty.
+    -----------------------------              -------------------------------
+    |              |             |            \ |              |             |
+    |     DATA     |     NEXT    |--------------|     DATA     |     NEXT    |
+    |              |             |            / |              |             |
+    ------------------------------              ------------------------------
+*/
+/*
+int main(){
+
+    List_t* mylist = (List_t*)malloc(sizeof(list_t));
+    insertFront(mylist, "myval 1");
+    insertFront(mylist, "myval 2");
+    insertFront(mylist, "myval 3");
+    insertFront(mylist, "myval 4");
+
+    insertRear(mylist, "insert rear 1");
+    insertRear(mylist, "target");
+    insertRear(mylist, "insert rear 2");
+
+    printf("print list\n");
+    printf("list length: %d\n",mylist->length);
+*/
+
+int find_fd_by_name(List_t* list, char* name){
+    node_t* ptr = list->head;
+    // for(ptr; ptr != NULL; ptr = ptr->next)
+    //     printf("node: %s\n", (char*)(ptr->value));
+
+    // char mytarget[] = "target";
+    for(ptr = list->head; ptr != NULL; ptr = ptr->next){
+        if(strcmp(ptr->value, name)==0){
+            printf("found\n");
+            // printf("it's next val is %s\n", ptr->next->value);
+            return ptr->fd;
+        }
+    }
+    printf("fd not found in the list\n");
+    return 0;
+}
+
+void insertFront(List_t* list, void* valref, int fd) {
+    if (list->length == 0)
+        list->head = NULL;
+
+    node_t** head = &(list->head);
+    node_t* new_node;
+    new_node = malloc(sizeof(node_t));
+
+    new_node->value = valref;
+    new_node->fd = fd;
+    new_node->next = *head;
+    *head = new_node;
+    list->length++; 
+}
+
+void insertRear(List_t* list, void* valref, int fd) {
+    if (list->length == 0) {
+        insertFront(list, valref, fd);
+        return;
+    }
+
+    node_t* head = list->head;
+    node_t* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = malloc(sizeof(node_t));
+    current->next->value = valref;
+    current->next->fd = fd;
+    current->next->next = NULL;
+    list->length++;
+}
+
+void insertInOrder(List_t* list, void* valref, int fd) {
+    if (list->length == 0) {
+        insertFront(list, valref, fd);
+        return;
+    }
+
+    node_t** head = &(list->head);
+    node_t* new_node;
+    new_node = malloc(sizeof(node_t));
+    new_node->value = valref;
+    new_node->fd = fd;
+    new_node->next = NULL;
+
+    if (list->comparator(new_node->value, (*head)->value) <= 0) {
+        new_node->next = *head;
+        *head = new_node;
+    } 
+    else if ((*head)->next == NULL){ 
+        (*head)->next = new_node;
+    }                                
+    else {
+        node_t* prev = *head;
+        node_t* current = prev->next;
+        while (current != NULL) {
+            if (list->comparator(new_node->value, current->value) > 0) {
+                if (current->next != NULL) {
+                    prev = current;
+                    current = current->next;
+                } else {
+                    current->next = new_node;
+                    break;
+                }
+            } else {
+                prev->next = new_node;
+                new_node->next = current;
+                break;
+            }
+        }
+    }
+    list->length++;
+}
+
+void* removeFront(List_t* list) {
+    node_t** head = &(list->head);
+    void* retval = NULL;
+    node_t* next_node = NULL;
+
+    if (list->length == 0) {
+        return NULL;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->value;
+    list->length--;
+
+    node_t* temp = *head;
+    *head = next_node;
+    free(temp);
+
+    return retval;
+}
+
+void* removeRear(List_t* list) {
+    if (list->length == 0) {
+        return NULL;
+    } else if (list->length == 1) {
+        return removeFront(list);
+    }
+
+    void* retval = NULL;
+    node_t* head = list->head;
+    node_t* current = head;
+
+    while (current->next->next != NULL) { 
+        current = current->next;
+    }
+
+    retval = current->next->value;
+    free(current->next);
+    current->next = NULL;
+
+    list->length--;
+
+    return retval;
+}
+
+/* indexed by 0 */
+void* removeByIndex(List_t* list, int index) {
+    if (list->length <= index) {
+        return NULL;
+    }
+
+    node_t** head = &(list->head);
+    void* retval = NULL;
+    node_t* current = *head;
+    node_t* prev = NULL;
+    int i = 0;
+
+    if (index == 0) {
+        retval = (*head)->value;
+        
+		node_t* temp = *head;
+        *head = current->next;
+        free(temp);
+        
+		list->length--;
+        return retval;
+    }
+
+    while (i++ != index) {
+        prev = current;
+        current = current->next;
+    }
+
+    prev->next = current->next;
+    retval = current->value;
+    free(current);
+
+    list->length--;
+
+    return retval;
+}
+
+/*
+void* removeByvalue(List_t* list, (void*) value) {
+    if (list->length == 0) {
+        return NULL;
+    }
+
+    node_t* temp = (node_t*)malloc(sizeof(node_t));
+    temp = list->head;
+    for(temp; temp != NULL; temp = temp->next){
+        if(strcmp(temp->next->value, value)==0)
+            break;
+    }
+    temp->next
+
+
+
+    list->length--;
+
+    return retval;
+}
+*/
+
+void deleteList(List_t* list) {
+    if (list->length == 0)
+        return;
+    while (list->head != NULL){
+        removeFront(list);
+    }
+    list->length = 0;
+}
---
+++ server.h
@@ -0,0 +1,68 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <arpa/inet.h>
+#include <getopt.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <signal.h>
+#include "protocol.h"
+
+#include "linkedList.h"
+
+#define BUFFER_SIZE 1024
+#define SA struct sockaddr
+#define NUMBER_JOB_BUF 100
+
+/* prototype. Stop and think once again before beginning implementation */
+typedef struct {
+    char* room_name;
+    char* room_creater;
+    char* user_list;
+    uint32_t user_count;
+} chat_room;
+
+typedef struct {
+    int client_fd;
+    petr_header header;
+    char* msg;
+} header_and_msg;
+
+
+/* sbuf_t: Bounded buffer used by the Sbuf package. */
+typedef struct {
+    header_and_msg* buf; /* Buffer array */
+    int n; /* Maximum number of slots */
+    int front; /* buf[(front+1)%n] is first item */
+    int rear; /* buf[rear%n] is last item */
+    sem_t mutex; /* Protects accesses to buf */
+    sem_t slots; /* Counts available slots */
+    sem_t items; /* Counts available items */
+} sbuf_t;
+
+// void *job_thread(void *vargp);
+void *job_thread(void* vargp);
+void Sem_init(sem_t *sem, int pshared, unsigned int value);
+void P(sem_t *sem);
+void V(sem_t *sem);
+void sbuf_init(sbuf_t *sp, int n);
+void sbuf_deinit(sbuf_t *sp);
+// void sbuf_insert(sbuf_t *sp, int item);
+void sbuf_insert(sbuf_t *sp, int client_fd, petr_header header, char* msg);
+header_and_msg sbuf_remove(sbuf_t *sp);
+
+/*
+ * Old one is: ... 
+ * void run_server(int server_port);
+ */
+void run_server(int server_port, int number_job_thread);
+
+#endif
